92d57b6089d1c67a848ab6afcd6639fbf63a8309
king <akash0960@proton.me>
2026-06-29 17:47:32 +0530

    Update Footer background image to footer-imagev3.png

--- luxury-scroll-site/src/components/layout/Footer.tsx
+++ luxury-scroll-site/src/components/layout/Footer.tsx
@@ -41,7 +41,7 @@ export function Footer() {
       {/* Background Mountain/Pines Landscape Sketch */}
       <div className="absolute inset-0 w-full h-full pointer-events-none select-none z-0 mix-blend-multiply opacity-8">
         <Image 
-          src="/images/footer-imagev2.png"
+          src="/images/footer-imagev3.png"
           alt="Landscape Sketch Background"
           fill
           sizes="100vw"
